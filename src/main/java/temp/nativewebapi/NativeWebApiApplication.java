ee8a0c5a047a3644c60c42491c16ec56b260e582
KimYoonMin <dbsalszz@naver.com>
2026-09-19 21:46:42 +0900

    replaced the project

---
+++ src/main/java/temp/nativewebapi/NativeWebApiApplication.java
@@ -0,0 +1,13 @@
+package temp.nativewebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NativeWebApiApplication.class, args);
+    }
+
+}
